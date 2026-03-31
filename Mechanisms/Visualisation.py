fcc96fc49929cf99b279feb470c246269e79401b
Iancullinane06 <roughdog01@gmail.com>
2026-03-31 12:36:43 +0100

    Fixed trailing whitespace

--- Mechanisms/Visualisation.py
+++ Mechanisms/Visualisation.py
@@ -123,7 +123,7 @@ def build_and_render_visualisations(
     fig_width = 16.0 * cols / max_dim
 
     fig_height = 12.0 * rows / max_dim * figure_height_scale
-    
+
     fig = plt.figure(figsize=(fig_width, fig_height))
     fig.patch.set_facecolor("white")
     gs = fig.add_gridspec(
